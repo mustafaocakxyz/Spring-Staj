f77c06c5ed1af9d366e962377cdae83f835ec657
mustafaocakxyz <mustafaocakxyz@gmail.com>
2025-07-18 09:16:27 +0300

    Hello name form and display implemented

---
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "Controller works!";
+    }
+
+    @GetMapping("/")
+    public String showForm() {
+        return "welcome";  // points to welcome.html
+    }
+
+    @PostMapping("/greet")
+    public String greet(@RequestParam String name, Model model) {
+        model.addAttribute("name", name);
+        return "welcome";
+    }
+}
